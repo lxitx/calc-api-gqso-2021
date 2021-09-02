c3892ea454a7453e5954d2d5edf23a30664790af
Leite <wildimyla.leite@hotmail.com>
2021-09-02 02:20:11 +0000

    Melhora tratamento de parâmetros Soma

--- calc-api/src/main/java/app/Soma.java
+++ calc-api/src/main/java/app/Soma.java
@@ -1,14 +1,36 @@
 package app;
 
 import io.jooby.annotations.*;
+import io.jooby.exception.*;
 
 public class Soma{
 
         @Path("/soma/{a}/{b}")
         @GET
         public String s(@PathParam String a, @PathParam Stringb){
-            double s = Double.parseDouble(a) + Double.parseDouble b);
-            return String.format(s);
+            try{
+                double s = Double.parseDouble(a) + Double.parseDouble (b);
+
+                if(a < 0){
+                    trow new BadRequestException(String.format("Parâmetro inválido:\%s", a));
+                    if(b < 0){
+                        trow new BadRequestException(String.format("Parâmetro inválido:\%s", b));
+                    }
+                }
+
+                if(b < 0){
+                    trow new BadRequestException(String.format("Parâmetro inválido:\%s", b));
+                    if(a < 0){
+                        trow new BadRequestException(String.format("Parâmetro inválido:\%s", a));
+                    }
+                }
+
+                return String.format("%.2f",s);
+            } catch(NumberException nfe){
+                trow new BadRequestException(String.format("Parâmetro inválido:\%s\%s",a, b));
+
+            }
+            
         }
 
 }
