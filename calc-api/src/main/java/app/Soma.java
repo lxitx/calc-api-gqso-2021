59007ccbdb38e8bf1bb2ec2af89b7c9c76b43496
Leite <wildimyla.leite@hotmail.com>
2021-09-07 19:27:36 +0000

    Corrigindo erros de sintaxe da classe Soma

--- calc-api/src/main/java/app/Soma.java
+++ calc-api/src/main/java/app/Soma.java
@@ -6,9 +6,9 @@ public class Soma{
 
         @Path("/soma/{a}/{b}")
         @GET
-        public String s(@PathParam String a, @PathParam Stringb){
-            double s = Double.parseDouble(a) + Double.parseDouble b);
-            return String.format(s);
+        public String s(@PathParam String a, @PathParam String b){
+            double s = Double.parseDouble(a) + Double.parseDouble (b);
+            return String.valueOf(s);
         }
 
 }
