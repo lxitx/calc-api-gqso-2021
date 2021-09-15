f93beb0100dbc73d2cc9742f60fe1a2c97129041
Leite <wildimyla.leite@hotmail.com>
2021-09-15 01:18:47 +0000

    Tratamento de exceção de valores grandes melhorada

--- calc-api/src/main/java/app/Soma.java
+++ calc-api/src/main/java/app/Soma.java
@@ -5,32 +5,20 @@ import io.jooby.exception.*;
 
 public class Soma{
 
-        @Path("/soma/{a}/{b}")
-        @GET
-        public String s(@PathParam String a, @PathParam Stringb){
-            try{
-                double s = Double.parseDouble(a) + Double.parseDouble (b);
 
-                if(a < 0){
-                    trow new BadRequestException(String.format("Parâmetro inválido:\%s", a));
-                    if(b < 0){
-                        trow new BadRequestException(String.format("Parâmetro inválido:\%s", b));
-                    }
-                }
+    @Path("/soma/{a}/{b}")
+    @GET
+    public String s(@PathParam String a, @PathParam String b){
+        double x = Math.sqrt(Double.parseDouble(a));
+        double y = Math.sqrt(Double.parseDouble(b));
 
-                if(b < 0){
-                    trow new BadRequestException(String.format("Parâmetro inválido:\%s", b));
-                    if(a < 0){
-                        trow new BadRequestException(String.format("Parâmetro inválido:\%s", a));
-                    }
-                }
+        try{
+            double s = x + y;
 
-                return String.format("%.2f",s);
-            } catch(NumberException nfe){
-                trow new BadRequestException(String.format("Parâmetro inválido:\%s\%s",a, b));
+            return String.format("%.2f",s);
 
-            }
-            
-        }
-
+        } catch(NumberFormatException nfe){
+            throw new BadRequestException(String.format("Parâmetro inválido"));
+        }    
+    }
 }
